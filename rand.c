47718678cac13bbd015332857e1fe2b91c117102
Jeff Mcneely <jeff.mcneely@firemon.com>
2020-05-24 20:56:14 -0500

    change

--- rand.c
+++ rand.c
@@ -2,12 +2,25 @@
 #include <stdlib.h>
 #include <limits.h>
 
+unsigned int rando(void);
+
 int main(void) {
+	unsigned int i;
+	for (i = 0; i<=100; i++) {
+		printf("%d\n", rando());
+	}
+	return 0;
+}
+unsigned int rando(void) {
+	static unsigned int i = 0;
+	return i++;
+}
+void rand_print(void) {
 	unsigned int i;
 	unsigned int j;
 	unsigned int randj;
 	printf("RAND_MAX: %d\n", RAND_MAX);
-	for(i = 1; i <= 100; i++) {
+	for(i = 1; i <= 10; i++) {
 		for (j = 0; j <= INT_MAX; j++) {
 			srand(j);
 			randj = rand();
@@ -18,7 +31,4 @@ int main(void) {
 		}
 
 	}
-
-
-	return 0;
 }
