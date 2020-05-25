7759f4838cc7a946e5530de377a09e8c40283bbd
Jeff Mcneely <jeff.mcneely@firemon.com>
2020-05-24 23:12:20 -0500

    complete

--- badsort.c
+++ badsort.c
@@ -10,8 +10,12 @@
 void build_test(void);
 unsigned int test[TEST_SIZE];
 unsigned int rrange(unsigned int min, unsigned int max);
+void printarray(unsigned int *array, int size);
+int issamearray(unsigned int *a, unsigned int *b, int size);
+int issorted(unsigned int *array, int size);
+
 int badsort(void);
-int debug = 2;
+int debug = 3;
 
 int main(void) {
 	unsigned long count=0;
@@ -19,20 +23,19 @@ int main(void) {
 	unsigned int iteration = 0;
 	if (debug > 0) {
 		int i;
-		printf("%5d", 0);
-		for(i=0; i<TEST_SIZE; i++) {
-			printf("%2d ", test[i]);
-		}
-		printf("\n");
+		printf("%5d:", 0);
+		printarray(test,TEST_SIZE);
+
 	}
 	while(1) {
 		iteration++;
-		sleep(1);
 		if (debug > 1) {
 			printf("%5d:", iteration);
 		}
-		if(!badsort()) {
+		if(badsort()) {
 			printf(" winner");
+			printf("original\n");
+			printarray(test,TEST_SIZE);
 			return 0;
 		}
 
@@ -41,8 +44,7 @@ int main(void) {
 }
 void build_test(void) {
 	unsigned int i;
-	time_t t;
-	srand((unsigned) time(&t));
+	srand((unsigned) clock());
 	for (i = 0; i<TEST_SIZE; i++) {
 		test[i] = rrange(1,TEST_RANGE_MAX);
 
@@ -52,19 +54,27 @@ int badsort(void) {
 	unsigned int badarray[TEST_SIZE];
 	unsigned int maxnum = 0;
 	int i;
-	time_t t;
-	srand((unsigned) time(&t));
+
+	srand((unsigned) clock());
 	for (i = 0; i<TEST_SIZE; i++) {
 		badarray[i] = rrange(1,TEST_RANGE_MAX);
 	}
-	if (debug > 1) {
-		for(i = 0; i<TEST_SIZE; i++) {
-			printf("%2d ", badarray[i]);
-		}
-		printf("\n");
-	}
-	return 0;
+	if (debug > 1)
+		printarray(badarray,TEST_SIZE);
+	if (!issorted(badarray,TEST_SIZE))
+		return 0;
+	if (!issamearray(badarray,test,TEST_SIZE))
+		return 0;
+	return 1;	
 	
+}
+void printarray(unsigned int *array, int size) {
+	int i=0;
+	for(i = 0; i < size; i++) {
+		printf("%-2d", array[i]);
+	}
+	printf("\n");
+
 }
 int issorted(unsigned int *array, int size) {
 	int i;
@@ -74,8 +84,9 @@ int issorted(unsigned int *array, int size) {
 		if (maxnum <= array[i])
 			maxnum = array[i];
 		else
-			return 1;
+			return 0;
 	}
+	return 1;
 
 }
 
@@ -83,3 +94,33 @@ unsigned int rrange(unsigned int min, unsigned int max) {
 	return rand() % (max + 1 - min);
 }
 
+int issamearray(unsigned int *a, unsigned int *b, int size) {
+	int i;
+	int same=1;
+	unsigned int *map;
+	unsigned int maxnum=0;
+
+	for(i=0; i<size; i++) {
+		if (maxnum < a[i]) maxnum = a[i];
+	}
+	if(debug>2) printf("maxnum %d\n", maxnum);
+
+	map = calloc(maxnum+1, sizeof(unsigned int));
+	for(i=0; i<size; i++) {
+		map[a[i]]++;
+	}
+	if(debug>2) printarray(map,maxnum+1);
+	for(i=0; i<size; i++) {
+		map[b[i]]--;
+	}
+
+	if(debug>2) printarray(map,maxnum+1);
+	for(i=0; i<maxnum+1; i++) {
+		if (map[i] != 0) {
+			same = 0;;
+			break;
+		}
+	}
+	return same;
+
+}
