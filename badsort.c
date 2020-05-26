3519db76848e6b94308158abeb698e025b8ff03e
Jeff Mcneely <jeff.mcneely@firemon.com>
2020-05-25 21:26:47 -0500

    change var types

--- badsort.c
+++ badsort.c
@@ -3,24 +3,25 @@
 #include <limits.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdint.h>
+
 
 #define TEST_SIZE 5
 #define TEST_RANGE_MAX 10
 void build_test(void);
-unsigned int test[TEST_SIZE];
-unsigned int rrange(unsigned int min, unsigned int max);
-void printarray(unsigned int *array, int size);
-int issamearray(unsigned int *a, unsigned int *b, int size);
-int issorted(unsigned int *array, int size);
+uint32_t test[TEST_SIZE];
+uint32_t rrange(uint32_t min, uint32_t max);
+void printarray(uint32_t *array, int size);
+int issamearray(uint32_t *a, uint32_t *b, int size);
+int issorted(uint32_t *array, int size);
 
 int badsort(void);
 int debug = 0;
 
 int main(void) {
 	build_test();
-	unsigned int iteration = 0;
+	uint32_t iteration = 0;
 	if (debug > 0) {
-		int i;
 		printf("%5d:", 0);
 		printarray(test,TEST_SIZE);
 
@@ -41,7 +42,7 @@ int main(void) {
 	return 0;
 }
 void build_test(void) {
-	unsigned int i;
+	uint32_t i;
 	srand((unsigned) clock());
 	for (i = 0; i<TEST_SIZE; i++) {
 		test[i] = rrange(1,TEST_RANGE_MAX);
@@ -49,8 +50,7 @@ void build_test(void) {
 	}
 }
 int badsort(void) {
-	unsigned int badarray[TEST_SIZE];
-	unsigned int maxnum = 0;
+	uint32_t badarray[TEST_SIZE];
 	int i;
 
 	srand((unsigned) clock());
@@ -66,7 +66,7 @@ int badsort(void) {
 	return 1;	
 	
 }
-void printarray(unsigned int *array, int size) {
+void printarray(uint32_t *array, int size) {
 	int i=0;
 	for(i = 0; i < size; i++) {
 		printf("%-2d", array[i]);
@@ -74,9 +74,9 @@ void printarray(unsigned int *array, int size) {
 	printf("\n");
 
 }
-int issorted(unsigned int *array, int size) {
+int issorted(uint32_t *array, int size) {
 	int i;
-	unsigned int maxnum=0;
+	uint32_t maxnum=0;
 	
 	for (i=0; i<size; i++) {
 		if (maxnum <= array[i])
@@ -88,22 +88,22 @@ int issorted(unsigned int *array, int size) {
 
 }
 
-unsigned int rrange(unsigned int min, unsigned int max) {
+uint32_t rrange(uint32_t min, uint32_t max) {
 	return rand() % (max + 1 - min);
 }
 
-int issamearray(unsigned int *a, unsigned int *b, int size) {
+int issamearray(uint32_t *a, uint32_t *b, int size) {
 	int i;
 	int same=1;
-	unsigned int *map;
-	unsigned int maxnum=0;
+	uint32_t *map;
+	uint32_t maxnum=0;
 
 	for(i=0; i<size; i++) {
 		if (maxnum < a[i]) maxnum = a[i];
 	}
 	if(debug>2) printf("maxnum %d\n", maxnum);
 
-	map = calloc(maxnum+1, sizeof(unsigned int));
+	map = calloc(maxnum+1, sizeof(uint32_t));
 	for(i=0; i<size; i++) {
 		map[a[i]]++;
 	}
